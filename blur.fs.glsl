ce9b1293c5573c040d2c3be061f433e20a92c154
Patrick Walton <pcwalton@mimiga.net>
2018-08-29 12:59:09 -0700

    wip

--- blur.fs.glsl
+++ blur.fs.glsl
@@ -15,6 +15,12 @@ in vec2 vTexCoord;
 
 out vec4 oFragColor;
 
+vec4 sampleTexture(float x0, vec2 k) {
+    float xMid = (x0 + k.y / (k.x + k.y)) / uSrcLength;
+    vec2 texCoord = uVertical ? vec2(vTexCoord.x, xMid) : vec2(xMid, vTexCoord.y);
+    return (k.x + k.y) * texture(uTexture, texCoord);
+}
+
 void main() {
     float center = (uVertical ? vTexCoord.y : vTexCoord.x) * uSrcLength;
     float start = floor(center - SUPPORT) + 0.5, end = ceil(center + SUPPORT) - 0.5;
@@ -22,15 +28,12 @@ void main() {
     vec4 colorSum = vec4(0.0);
     float factorSum = 0.0;
 
-    for (float x = start; x <= end; x += 1.0) {
-        float texX = x / uSrcLength;
-        vec2 texCoord = uVertical ? vec2(vTexCoord.x, texX) : vec2(texX, vTexCoord.y);
+    for (float x = start; x <= end; x += 2.0) {
+        vec2 offsets = vec2(0.0, 1.0) + x - center;
+        vec2 factors = exp(uCoeff * offsets * offsets);
 
-        float offset = x - center;
-        float factor = exp(uCoeff * offset * offset);
-
-        colorSum += texture(uTexture, texCoord) * factor;
-        factorSum += factor;
+        colorSum += sampleTexture(x, factors);
+        factorSum += factors.x + factors.y;
     }
 
     oFragColor = colorSum / factorSum;
