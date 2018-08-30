d7475fffd11155a37f039dc88f55d1880cbfe880
Patrick Walton <pcwalton@mimiga.net>
2018-08-30 10:36:32 -0700

    Less support

--- blur.fs.glsl
+++ blur.fs.glsl
@@ -4,7 +4,7 @@
 
 precision highp float;
 
-#define SUPPORT 6.0
+#define SUPPORT 4.0
 
 uniform sampler2D uTexture;
 uniform vec2 uSrcSize;
@@ -15,6 +15,18 @@ in vec2 vTexCoord;
 
 out vec4 oFragColor;
 
+void accum(float x, float center, float srcLength, inout vec4 colorSum, inout float factorSum) {
+    vec2 offsets = vec2(0.0, 1.0) + x - center;
+    vec2 factors = exp(uCoeff * offsets * offsets);
+
+    float bothFactors = factors.x + factors.y;
+    float xMid = (x + factors.y / bothFactors) / srcLength;
+    vec2 texCoord = uVertical ? vec2(vTexCoord.x, xMid) : vec2(xMid, vTexCoord.y);
+
+    colorSum += bothFactors * texture(uTexture, texCoord);
+    factorSum += bothFactors;
+}
+
 void main() {
     float srcLength = uVertical ? uSrcSize.y : uSrcSize.x;
     float center = (uVertical ? vTexCoord.y : vTexCoord.x) * srcLength;
@@ -23,17 +35,11 @@ void main() {
     vec4 colorSum = vec4(0.0);
     float factorSum = 0.0;
 
-    for (float x = start; x <= end; x += 2.0) {
-        vec2 offsets = vec2(0.0, 1.0) + x - center;
-        vec2 factors = exp(uCoeff * offsets * offsets);
-
-        float bothFactors = factors.x + factors.y;
-        float xMid = (x + factors.y / bothFactors) / srcLength;
-        vec2 texCoord = uVertical ? vec2(vTexCoord.x, xMid) : vec2(xMid, vTexCoord.y);
-
-        colorSum += bothFactors * texture(uTexture, texCoord);
-        factorSum += bothFactors;
-    }
+    accum(start,        center, srcLength, colorSum, factorSum);
+    accum(start +  2.0, center, srcLength, colorSum, factorSum);
+    accum(start +  4.0, center, srcLength, colorSum, factorSum);
+    accum(start +  6.0, center, srcLength, colorSum, factorSum);
+    accum(start +  8.0, center, srcLength, colorSum, factorSum);
 
     oFragColor = colorSum / factorSum;
 }
